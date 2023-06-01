80504b7e503c123a2ffe58d23c713676269c6e7a
Demid ElAmin <smeed21@yandex.ru>
2023-06-02 02:18:01 +0300

    Проект 1. Попытка 3.

--- orders_view.sql
+++ orders_view.sql
@@ -1,10 +1,13 @@
-CREATE OR REPLACE VIEW analysis.orders AS
-SELECT o.order_id, o.order_ts, o.user_id, o.bonus_payment,
-o.payment, o.cost, o.bonus_grant ,ol.status_id AS status
-FROM analysis.orders AS o 
-RIGHT JOIN (
-	SELECT order_id, status_id, MAX(dttm) AS "date"
-	FROM analysis.orderstatuslog
-	GROUP BY order_id, status_id)
-AS ol ON o.order_id = ol.order_id
-WHERE "date"=order_ts
+-- удаляем старое представление. логика описана в txt файле
+-- DROP VIEW analysis.orders;
+
+-- создаем новое представление с помощью уже двух таблиц
+CREATE VIEW analysis.orders AS
+SELECT o.order_id, o.order_ts, o.user_id, o.bonus_payment, 
+o.payment, o.cost, o.bonus_grant ,a.status_id, o.status
+FROM (SELECT	order_id ,status_id ,dttm
+		,max(dttm) OVER(PARTITION BY order_id) AS max_dt
+	FROM production.orderstatuslog
+	ORDER  BY order_id) AS a 
+JOIN production.orders AS o ON a.order_id=o.order_id
+WHERE dttm=max_dt
