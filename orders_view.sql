9e518c6c9cb50f127313a1591e4d82acdba11bba
Demid ElAmin <smeed21@yandex.ru>
2023-06-02 02:31:14 +0300

    Проект 1. Попытка 3.1

--- orders_view.sql
+++ orders_view.sql
@@ -4,7 +4,7 @@
 -- создаем новое представление с помощью уже двух таблиц
 CREATE VIEW analysis.orders AS
 SELECT o.order_id, o.order_ts, o.user_id, o.bonus_payment, 
-o.payment, o.cost, o.bonus_grant ,a.status_id, o.status
+o.payment, o.cost, o.bonus_grant ,a.status_id
 FROM (SELECT	order_id ,status_id ,dttm
 		,max(dttm) OVER(PARTITION BY order_id) AS max_dt
 	FROM production.orderstatuslog
